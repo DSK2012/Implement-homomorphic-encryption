fac14528f36c2b80628f8dda8041baabea21da00
DSK2012 <42681371+DSK2012@users.noreply.github.com>
2020-05-30 23:58:27 +0530

    Create rsa.py

---
+++ rsa.py
@@ -0,0 +1,34 @@
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
+
+
+def enc(dat):
+    data = str(dat).encode()
+    cipher_rsa = PKCS1_OAEP.new(key)
+    enc_data = cipher_rsa.encrypt(data)
+    arr1.append(enc_data)
+
+def dec(dat):
+    cipher_rsa = PKCS1_OAEP.new(key)
+    data_dec = cipher_rsa.decrypt(dat)
+    arr2.append(int(data_dec))
+
+if __name__ == "__main__":
+    dict = {}
+
+    key = RSA.generate(2048)
+    arr = [1,2,3,4,5]
+    arr1 = []
+    arr2 = []
+
+    dict['publickey'] = key.publickey().export_key()
+    dict['privatekey'] = key.export_key()
+    dict['data'] = arr
+    print(dict,"\n\n")
+    for i in arr:
+        enc(i)
+    print(arr1,"\n\n")
+    for i in arr1:
+        dec(i)
+    print(arr2)
